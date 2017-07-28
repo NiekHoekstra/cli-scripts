6fb600fc14ed4b48b3d11bc96518b0723b46a2c2
Niek Hoekstra <niek.hoekstra@target-holding.nl>
2017-07-28 13:40:12 +0200

    Added gitroot script

---
+++ git/gitroot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# alias in .bashrc
+# alias gitroot=$(gitroot.sh)
+
+git status &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "echo Not in git repository."
+  exit 1
+fi
+
+while [ ! -d ".git" ]; do
+ cd ..
+done
+dir=`pwd`
+echo "cd $dir"
